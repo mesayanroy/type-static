fed8adf0d0811d547611e8ac22158b390a1a85ed
Sayan Roy <169074736+mesayanroy@users.noreply.github.com>
2025-11-08 02:16:30 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var a = 12;
+function test() {
+    let x = 10;
+    let y = 20;
+}
+//# sourceMappingURL=app.js.map
---
+++ app.ts
@@ -0,0 +1,6 @@
+var a = 12;
+
+function test() {
+    let x = 10;
+    let y = 20;
+}
